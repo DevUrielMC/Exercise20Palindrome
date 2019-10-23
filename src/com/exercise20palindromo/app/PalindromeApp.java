a18d163437613f54848c1218ad7378e77a2b6571
DevUrielMC <urimarcervantes.95@gmail.com>
2019-10-23 13:06:49 -0500

    Ejercicio terminado

--- src/com/exercise20palindromo/app/PalindromeApp.java
+++ src/com/exercise20palindromo/app/PalindromeApp.java
@@ -1,12 +1,26 @@
 package com.exercise20palindromo.app;
 
+import com.exercise20palindromo.model.StringAplications;
+
 public class PalindromeApp 
 {
 
 	public static void main(String[] args) 
 	{
+		String cadPalindrome1 ="Anita lava la tina.";
+		String cadPalindrome2 ="Yo dono rosas, oro no doy.!!";
+		String cadPalindrome3 ="Isaac no ronca asi";
 		
-
+		//forma1
+		System.out.println(StringAplications.isPalindrome(cadPalindrome1));
+		System.out.println(StringAplications.isPalindrome(cadPalindrome2));
+		System.out.println(StringAplications.isPalindrome(cadPalindrome3));
+		//forma2
+		System.out.println(StringAplications.isPalindromeStack(cadPalindrome1));
+		System.out.println(StringAplications.isPalindromeStack(cadPalindrome2));
+		System.out.println(StringAplications.isPalindromeStack(cadPalindrome3));
+		
 	}
 
 }
+
